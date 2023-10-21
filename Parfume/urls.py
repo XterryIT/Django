9808fdd9b7f74f94bfa5514524c5150a73d702d2
XterryIT <vptashka2@gmail.com>
2023-10-21 22:30:14 +0200

    began creation database

--- Parfume/urls.py
+++ Parfume/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('product/', include('data.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
