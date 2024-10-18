37f06ac8c194492500980fa3b624b289ca22228f
XterryIT <vptashka2@gmail.com>
2024-10-18 16:11:26 +0200

    Finish with models for my course

--- educa/courses/admin.py
+++ educa/courses/admin.py
@@ -6,11 +6,13 @@ from .models import Subject, Course, Module
 class SubjectAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug']
     prepopulated_fields = {'slug': ('title',)}
+
+    
 class ModuleInline(admin.StackedInline):
     model = Module
 
 
-    
+
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['title', 'subject', 'created']
